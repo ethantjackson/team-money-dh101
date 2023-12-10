0aac214a06d468d437a5672a0df6124d124454e2
ethantjackson <ethantjackson44@gmail.com>
2023-12-09 23:42:17 -0800

    add data critique

--- src/DataCritique.js
+++ src/DataCritique.js
@@ -1,169 +1,204 @@
-import GetAppIcon from "@mui/icons-material/GetApp";
-import { Box, Button, Container, Typography } from "@mui/material";
-import React from "react";
-import Navbar from "./Navbar";
+import { Box, Container, Link, Typography } from '@mui/material';
+import React from 'react';
+import Navbar from './Navbar';
 
 const DataCritique = () => {
   // Define a custom style for the yellow divider
-  const dividerStyle = {
-    height: "5px",
-    marginY: 4, // adds margin to the top and bottom
-  };
 
   return (
     <>
       <Navbar />
-      <Box sx={{ backgroundColor: "#f5f5f5", paddingY: 4 }}>
-        <Container sx={{ textAlign: "center" }}>
-          <Typography variant="h3" sx={{ fontWeight: "bold", marginBottom: 2 }}>
-            Data / Critique
+      <Container
+        sx={{
+          paddingTop: '50px',
+          paddingBottom: '50px',
+        }}
+      >
+        <Typography
+          variant='h2'
+          mt={3}
+          mb={2}
+          sx={{
+            fontWeight: 'bold',
+          }}
+        >
+          Data critiques and considerations
+        </Typography>
+        <Typography mb={2}>
+          Here, we explore the datasets from which we have drawn our narratives,
+          and we acknowledge the latent silences that might exist in the data.
+          Feel free to{' '}
+          <Link href='https://genderdata.worldbank.org/'>
+            explore the Gender Data Portal
+          </Link>
+          .
+        </Typography>
+        <Box sx={{ backgroundColor: 'white' }} pt={1} pb={2} pl={3} pr={3}>
+          <Typography variant='h3' sx={{ fontWeight: 'bold' }} mt={1} mb={1}>
+            About the database
           </Typography>
-          <Typography paragraph>
-            This page lists all the sources used throughout this website as well as a few details on
-            the technical tools we used to make this project. You can also download our annotated
-            bibliography if you would like an overview & analysis of select sources.
+          <Typography>
+            The World Bank is an international development organization (IGO)
+            owned by 187 countries. Its role is to reduce poverty by lending
+            money to the governments of its poorer members to improve their
+            economies and to improve the standard of living of their people. The
+            organization’s “Gender Portal” brings attention to the gender gap by
+            making their statistics accessible and understandable through data
+            visualizations.
+            <br />
+            <br />
+            The World Bank’s Strengthening Gender Statistics (SGS) project
+            conducts assessments that center around economic indicators across
+            three key areas: ownership and control of assets, employment, and
+            entrepreneurship. Some of these indicators come from the NSO’s
+            (National Statistical Office) published annual statistical
+            yearbooks, survey reports of major surveys or census, published
+            gender factbooks or compendiums, and SDG (Sustainable Development
+            Goals) data and results. When conducting a gender data gap
+            assessment, The World Bank identifies and computes these indicators
+            into documented metadata to ensure that the information is relevant,
+            meets standards, and to recognize discrepancies.
+            <br />
+            <br />
+            Although awareness of male dominance and privilege in society has
+            increased tremendously, we see how structural determinants of
+            patriarchy have critical roles in inequalities, one of these
+            inequalities being wealth. The disparity in wealth is an issue that
+            goes beyond income difference but also brings attention to
+            structural inequalities within a society such as resources and
+            opportunities that affect the lack of economic equality. When
+            exploring the relationships between socioeconomic factors and wealth
+            inequality between genders, it is important to consider and identify
+            the patterns over time so that we can see how wealth inequality has
+            evolved. While factors that contribute to wealth inequality are
+            dynamic and diverse, identifying these trends and the history behind
+            them can play an important role in understanding what causes wealth
+            inequality and measures we can put in place to minimize the wealth
+            gap. Societal nuances such as a country’s placement on the
+            individualism/collectivism spectrum are also important as they
+            signify whether there are cultural and traditional influences that
+            impact wealth inequality; directly or indirectly. More personal
+            factors like education, contribute to wealth inequality, and
+            understanding how significant these factors are in impacting the
+            acquisition of wealth holds equal importance. The World Bank, an
+            institution dedicated to reducing poverty around the world, provides
+            datasets that reveal correlations between the gender gap and various
+            measures of wealth such as assets and employment and time use. The
+            credibility and efficiency of this source is reflected in The World
+            Bank’s ability to access international assessments and economic
+            indicators. Through various checkpoints such as relevancy,
+            standards, and discrepancies, this information is reviewed and
+            documented into metadata that argues the seriousness of the gender
+            gap.
           </Typography>
-          <Button
-            variant="contained"
-            sx={{
-              marginTop: 2,
-              color: "common.white",
-              backgroundColor: "primary.main",
-              "&:hover": {
-                backgroundColor: "primary.dark",
-              },
-            }}
-            startIcon={<GetAppIcon />}
-            href="#dummy-link"
-          >
-            Explore the full database
-          </Button>
-        </Container>
-      </Box>
-
-      {/* Yellow Divider */}
-      <Box sx={dividerStyle} />
-
-      <Box sx={{ backgroundColor: "#f5f5f5", paddingY: 4 }}>
-        <Container>
-          <Typography variant="h4" sx={{ fontWeight: "bold", marginBottom: 1 }} gutterBottom>
-            About the Database
+          <Typography variant='h3' sx={{ fontWeight: 'bold' }} mt={3} mb={1}>
+            What information is included in the dataset?
           </Typography>
-        </Container>
-      </Box>
-
-      {/* Yellow Divider */}
-      <Box sx={dividerStyle} />
-
-      {/* SOURCES Section */}
-      <Box sx={{ backgroundColor: "#f5f5f5", paddingY: 4 }}>
-        <Container>
-          <Typography variant="h4" sx={{ fontWeight: "bold", marginBottom: 1 }} gutterBottom>
-            Data Critique
+          <Typography>
+            The dataset is composed of many economic indicators that organize
+            important statistics on ownership and control of assets, employment,
+            and entrepreneurship. The dataset provides a CSV file that is
+            divided into five columns: Indicator Name, Indicator Code, Country
+            Name, Country Code, Year, and Value.
+            <ul>
+              <li>
+                The <b>Indicator Name </b>identifies what the data is concerned
+                with; what is being measured (e.g. “Female Percentage Working
+                in...")
+              </li>
+              <li>
+                The <b>Indicator Code</b> is a series of eleven letters that
+                organizes the information
+              </li>
+              <li>
+                The <b>Country Name </b>is the name of the country being
+                measured
+              </li>
+              <li>
+                The <b>Country Code</b> is a series of three letters that
+                identifies the country and its location
+              </li>
+              <li>
+                The <b>Year</b> is the time frame in which the data comes from
+                and represents
+              </li>
+              <li>
+                The <b>Value</b> is a series of numbers that reflect the value
+                of the statistic
+              </li>
+            </ul>
           </Typography>
-          <Typography paragraph>
-            Before we could create an argument with data, we needed to critically analyze the
-            politics of data collection processes & outcomes.
+          <Typography variant='h3' sx={{ fontWeight: 'bold' }} mt={3} mb={1}>
+            What the dataset illuminates
           </Typography>
-          <Typography variant="h4" sx={{ fontWeight: "bold", marginBottom: 1 }} gutterBottom>
-            How was the data generated?
+          <Typography>
+            The Gender Data Portal is a dataset that, through different metadata
+            created by indicators deemed economically significant, provides
+            important statistics that can be analyzed to form correlations.
+            Firstly, it addresses the evolving dynamics of wealth inequality
+            between genders over time, a topic that has not been thoroughly
+            explored in existing literature. By analyzing this trend, the
+            dataset helps us to understand whether gender-based wealth
+            disparities are widening or narrowing, offering fresh insights into
+            the effectiveness of current gender equality initiatives. Secondly,
+            our research delves into the influence of a country's cultural
+            orientation on gender wealth inequality. This aspect is crucial as
+            it opens up a new dimension in understanding how societal values,
+            particularly individualism versus collectivism, play a role in
+            shaping gendered economic experiences. Finally, the impact of
+            education on wealth acquisition, particularly in a gendered context,
+            forms a critical part of our study. This is vital because it sheds
+            light on the long-debated question of education's role as an
+            equalizer in society, especially concerning gendered wealth
+            disparities. Overall, the dataset illuminates and facilitates a
+            deeper understanding of these complex dynamics, ultimately helping
+            policymakers, educators, and activists to formulate more effective
+            strategies for promoting gender equality in wealth distribution.
           </Typography>
-          What incentive is there for The Bureau to investigate the drone campaign? What are the
-          stakes & who are the stakeholders?
-          <Typography variant="h4" sx={{ fontWeight: "bold", marginBottom: 1 }} gutterBottom>
-            What are the original sources?
+          <Typography variant='h3' sx={{ fontWeight: 'bold' }} mt={3} mb={1}>
+            Where the dataset is limited
           </Typography>
-          What incentive is there for The Bureau to investigate the drone campaign? What are the
-          stakes & who are the stakeholders?
-          <Typography variant="h4" sx={{ fontWeight: "bold", marginBottom: 1 }} gutterBottom>
-            What information is left out?
+          <Typography>
+            Although the dataset can demonstrate a gender gap in various
+            measures of wealth (land ownership and employment), it cannot reveal
+            a causal relationship between any driver of wealth inequality and
+            the inequality itself.
+            <br />
+            <br /> The datasets themselves only represent documented people and
+            don't cover a hundred percent of the global population.
+            Additionally, there is no documented non-responses, which adds bias
+            to the dataset based on who actually responded among those
+            questioned. Analyzing with a political lens, we can also conclude
+            that data could be manipulated to create a narrative that a region
+            wants to portray. Therefore, there is a limitation in that the data
+            might not reflect a true representation of a nation.
+            <br />
+            <br /> The World Bank also discusses lack of data availability
+            through interactive data visualizations on “Global View,” “Topic
+            View,” and “Availability by Indicator” that shows slow, but
+            important, progress on improving access and data.
           </Typography>
-          What incentive is there for The Bureau to investigate the drone campaign? What are the
-          stakes & who are the stakeholders?
-          <Typography variant="h4" sx={{ fontWeight: "bold", marginBottom: 1 }} gutterBottom>
-            Who does the dataset represent?
+          <Typography variant='h3' sx={{ fontWeight: 'bold' }} mt={3} mb={1}>
+            Dataset ontologies
           </Typography>
-          What incentive is there for The Bureau to investigate the drone campaign? What are the
-          stakes & who are the stakeholders?
-        </Container>
-      </Box>
-
-      {/* PROCESSING Section */}
-      <Box sx={{ backgroundColor: "#f5f5f5", paddingY: 4 }}>
-        <Container>
-          <Typography variant="h4" sx={{ fontWeight: "bold", marginBottom: 1 }} gutterBottom>
-            PROCESSING
+          <Typography>
+            If our data sets were our only source of information, there would be
+            a lot of external factors that would have been left out that we
+            would not be able to take into account when analyzing. Some external
+            factors that we would not be able to see are personal history or the
+            origins of the data. This is important because many countries have
+            different gender norms, cultural expectations, and legislation that
+            may affect the way one acquires wealth. There are also underlying
+            variables that we would likely be unable to uncover, similar to the
+            one about laws that directly affect wealth generation and
+            acquisition. Using one data set can also restrict our fact-checking
+            abilities as we would be unable to cross-reference our findings with
+            more data. If there was another data set that provided new or
+            contradictory insights from ours, we would not be able to include
+            and analyze the discrepancies therein.
           </Typography>
-          <Typography paragraph>
-            MEOW MEOW Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis interdum
-            facilisis tristique. Vivamus vitae pretium nisi, eget malesuada felis. Mauris consequat
-            justo imperdiet elit dapibus laoreet. Pellentesque condimentum leo sed lobortis aliquet.
-            Donec euismod non mauris sit amet congue. In eget massa sit amet ligula ullamcorper
-            venenatis dignissim ultricies velit. Aliquam sit amet dui consequat, feugiat nisi non,
-            fermentum lacus. Vestibulum feugiat fermentum varius. Sed rutrum purus et mi consequat
-            hendrerit. Maecenas libero diam, tristique sed tortor non, luctus molestie erat.
-            Pellentesque consectetur eros vitae purus varius, et pharetra tellus dictum.
-            Pellentesque fermentum malesuada urna vel consectetur. Aliquam viverra sapien ut
-            bibendum tincidunt. Pellentesque ultricies nisl justo.
-          </Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis interdum facilisis
-          tristique. Vivamus vitae pretium nisi, eget malesuada felis. Mauris consequat justo
-          imperdiet elit dapibus laoreet. Pellentesque condimentum leo sed lobortis aliquet. Donec
-          euismod non mauris sit amet congue. In eget massa sit amet ligula ullamcorper venenatis
-          dignissim ultricies velit. Aliquam sit amet dui consequat, feugiat nisi non, fermentum
-          lacus. Vestibulum feugiat fermentum varius. Sed rutrum purus et mi consequat hendrerit.
-          Maecenas libero diam, tristique sed tortor non, luctus molestie erat. Pellentesque
-          consectetur eros vitae purus varius, et pharetra tellus dictum. Pellentesque fermentum
-          malesuada urna vel consectetur. Aliquam viverra sapien ut bibendum tincidunt. Pellentesque
-          ultricies nisl justo.
-        </Container>
-      </Box>
-
-      {/* Yellow Divider */}
-      <Box sx={dividerStyle} />
-
-      {/* PRESENTATION Section */}
-      <Box sx={{ backgroundColor: "#f5f5f5", paddingY: 4 }}>
-        <Container>
-          <Typography variant="h4" sx={{ fontWeight: "bold", marginBottom: 1 }} gutterBottom>
-            PRESENTATION
-          </Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis interdum facilisis
-          tristique. Vivamus vitae pretium nisi, eget malesuada felis. Mauris consequat justo
-          imperdiet elit dapibus laoreet. Pellentesque condimentum leo sed lobortis aliquet. Donec
-          euismod non mauris sit amet congue. In eget massa sit amet ligula ullamcorper venenatis
-          dignissim ultricies velit. Aliquam sit amet dui consequat, feugiat nisi non, fermentum
-          lacus. Vestibulum feugiat fermentum varius. Sed rutrum purus et mi consequat hendrerit.
-          Maecenas libero diam, tristique sed tortor non, luctus molestie erat. Pellentesque
-          consectetur eros vitae purus varius, et pharetra tellus dictum. Pellentesque fermentum
-          malesuada urna vel consectetur. Aliquam viverra sapien ut bibendum tincidunt. Pellentesque
-          ultricies nisl justo.
-        </Container>
-      </Box>
-
-      {/* Yellow Divider */}
-      <Box sx={dividerStyle} />
-
-      {/* Works Cited */}
-      <Box sx={{ backgroundColor: "#f5f5f5", paddingY: 4 }}>
-        <Container>
-          <Typography variant="h4" sx={{ fontWeight: "bold", marginBottom: 1 }} gutterBottom>
-            Works Cited
-          </Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis interdum facilisis
-          tristique. Vivamus vitae pretium nisi, eget malesuada felis. Mauris consequat justo
-          imperdiet elit dapibus laoreet. Pellentesque condimentum leo sed lobortis aliquet. Donec
-          euismod non mauris sit amet congue. In eget massa sit amet ligula ullamcorper venenatis
-          dignissim ultricies velit. Aliquam sit amet dui consequat, feugiat nisi non, fermentum
-          lacus. Vestibulum feugiat fermentum varius. Sed rutrum purus et mi consequat hendrerit.
-          Maecenas libero diam, tristique sed tortor non, luctus molestie erat. Pellentesque
-          consectetur eros vitae purus varius, et pharetra tellus dictum. Pellentesque fermentum
-          malesuada urna vel consectetur. Aliquam viverra sapien ut bibendum tincidunt. Pellentesque
-          ultricies nisl justo.
-        </Container>
-      </Box>
-
-      {/* Add additional sections as needed, followed by a yellow divider */}
+        </Box>
+      </Container>
     </>
   );
 };
